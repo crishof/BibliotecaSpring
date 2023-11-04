a52d7394174f5ef03d16639277407494fab1e4ed
Cristian.hof <cristianhof@gmail.com>
2023-11-04 16:25:38 +0100

    delete noticias

--- src/main/java/com/egg/bibliotecaSpring/repositorios/NoticiaRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.egg.bibliotecaSpring.repositorios;
-
-import com.egg.bibliotecaSpring.entidades.Noticia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticiaRepositorio extends JpaRepository<Noticia, Long> {
-}
